ea48d7d7450103ce66d10090a2993ba01fdd22d2
TerezaZdislava <bastova.ter@gmail.com>
2020-11-17 20:13:17 +0100

    uprava dat

--- src/FormActivity.js
+++ src/FormActivity.js
@@ -1,141 +1,47 @@
 import React, { useState } from 'react';
 import { Formik, Field, Form } from 'formik';
 
-// const questions = [
-//   {
-//     question: 'Kolik jídel denně preferuješ?',
-//     choices: [
-//       { option: 'A', answer: '3 jídla (snídaně, oběd, večeře)' },
-//       { option: 'A', answer: '5 jídel (svačiny mezi hlavními jídly)' },
-//     ],
-//   },
-//   {
-//     question: 'Sportuješ během týdne?',
-//     choices: [
-//       { option: 'A', answer: 'Nesportuji.' },
-//       { option: 'B', answer: 'Sportuji 1 - 2x týdně.' },
-//       { option: 'C', answer: 'Sportuji 3 - 4x týdně.' },
-//       {
-//         option: 'D',
-//         answer: 'Jsem sportovní nadšenec a dám si do těla minimálně 5x týdně.',
-//       },
-//     ],
-//   },
-//   {
-//     question: 'Jaké je Vaše zaměstnání, popřípadě náplň dne?',
-//     choices: [
-//       { option: 'A', answer: 'Nejsem aktivní. Denně ujdu méně než 8 K kroků.' },
-//       {
-//         option: 'B',
-//         answer: 'Jsem středně aktivní, nachodím kolem 8 K kroků.',
-//       },
-//       {
-//         option: 'C',
-//         answer: 'Jsem aktivní a většinu dne na nohou. Nachodím nad 10 K kroků',
-//       },
-//     ],
-//   },
-// ];
-
-// class Questions extends React.Component {
-// //const FormActivity = ({ formData, setFormData, nextStep, prevStep }) => {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       questions: [],
-//     };
-//   }
-
-//   userSelection(length) {
-//     const initialValues = [];
-//     for (let i = 0; i < length; i++) {
-//       initialValues.push({ question: i, answer: '' });
-//     }
-
-//     return initialValues;
-//   }
-
-//   componentDidMount() {
-//     // Fetch all the questions and set state
-//     this.setState({ questions });
-//   }
-
-//   render() {
-//     const { questions } = this.state;
-//     const userSelection = { answers: this.userSelection(questions.length) };
-//     console.log(userSelection);
-//     const [direction, setDirection] = useState('back');
-
-//     return (
-//       <Formik enableReinitialize={true} initialValues={userSelection}>
-//         {({ values, handleBlur, handleChange, handleSubmit, isSubmitting }) => (
-//           <form onSubmit={handleSubmit}>
-//             {questions.map((q, index) => (
-//               <div className="card" key={index}>
-//                 {/* <div className="card-header">Hádej</div> */}
-//                 <div className="card-body">
-//                   <h6 className="card-title">{q.question}</h6>
-//                   <div className="question-choices px-2">
-//                     {q.choices.map((choice) => (
-//                       <div className="form-check" key={choice.option}>
-//                         <input
-//                           type="radio"
-//                           id={choice.option}
-//                           className="form-check-input"
-//                           name={`answers[${index}].answer`}
-//                           value={choice.option}
-//                           checked={
-//                             values.answers && values.answers[index]
-//                               ? values.answers[index].answer === choice.option
-//                               : false
-//                           }
-//                           onChange={handleChange}
-//                           onBlur={handleBlur}
-//                         />
-//                         <label
-//                           className="form-check-label clickable"
-//                           htmlFor={choice.option}
-//                         >
-//                           <span className="mr-2">{choice.option} )</span>{' '}
-//                           {choice.answer}
-//                         </label>
-//                       </div>
-//                     ))}
-//                   </div>
-//                 </div>
-//               </div>
-//             ))}
-//             <pre>{JSON.stringify(values, null, 2)}</pre>
-//             <div>
-//               <button
-//                 type="submit"
-//                 variant="contained"
-//                 onClick={() => setDirection('back')}
-//               >
-//                 Back
-//               </button>
-//               <button
-//                 type="submit"
-//                 variant="contained"
-//                 onClick={() => setDirection('forward')}
-//               >
-//                 Continue
-//               </button>
-//             </div>
-//           </form>
-//         )}
-//       </Formik>
-//     );
-//   }
-// }
+const questions = [
+  {
+    question: 'Kolik jídel denně preferuješ?',
+    choices: [
+      { option: 'A', answer: '3 jídla (snídaně, oběd, večeře)' },
+      { option: 'A', answer: '5 jídel (svačiny mezi hlavními jídly)' },
+    ],
+  },
+  {
+    question: 'Sportuješ během týdne?',
+    choices: [
+      { option: 'A', answer: 'Nesportuji.' },
+      { option: 'B', answer: 'Sportuji 1 - 2x týdně.' },
+      { option: 'C', answer: 'Sportuji 3 - 4x týdně.' },
+      {
+        option: 'D',
+        answer: 'Jsem sportovní nadšenec a dám si do těla minimálně 5x týdně.',
+      },
+    ],
+  },
+  {
+    question: 'Jaké je Vaše zaměstnání, popřípadě náplň dne?',
+    choices: [
+      {
+        option: 'A',
+        answer: 'Nejsem aktivní. Denně ujdu méně než 8 K kroků.',
+      },
+      {
+        option: 'B',
+        answer: 'Jsem středně aktivní, nachodím kolem 8 K kroků.',
+      },
+      {
+        option: 'C',
+        answer: 'Jsem aktivní a většinu dne na nohou. Nachodím nad 10 K kroků',
+      },
+    ],
+  },
+];
 
 const FormActivity = ({ formData, setFormData, nextStep, prevStep }) => {
-  // const userSelection = {
-  //   answers: this.userSelection(formData.form3.questions.length),
-  // };
   const [direction, setDirection] = useState('back');
-
   return (
     // <Formik enableReinitialize={true} initialValues={userSelection}>
     //   {({ values, handleBlur, handleChange, handleSubmit, isSubmitting }) => (
@@ -149,7 +55,7 @@ const FormActivity = ({ formData, setFormData, nextStep, prevStep }) => {
     >
       {({ values, handleBlur, handleChange }) => (
         <Form>
-          {formData.form3.questions.map((q, index) => (
+          {questions.map((q, index) => (
             <div className="card" key={index}>
               {/* <div className="card-header">Hádej</div> */}
               <div className="card-body">
--- src/Formparent.js
+++ src/Formparent.js
@@ -15,48 +15,6 @@ export const FormParent = () => {
       weight: '',
     },
     form2: {},
-    form3: {
-      questions: [
-        {
-          question: 'Kolik jídel denně preferuješ?',
-          choices: [
-            { option: 'A', answer: '3 jídla (snídaně, oběd, večeře)' },
-            { option: 'A', answer: '5 jídel (svačiny mezi hlavními jídly)' },
-          ],
-        },
-        {
-          question: 'Sportuješ během týdne?',
-          choices: [
-            { option: 'A', answer: 'Nesportuji.' },
-            { option: 'B', answer: 'Sportuji 1 - 2x týdně.' },
-            { option: 'C', answer: 'Sportuji 3 - 4x týdně.' },
-            {
-              option: 'D',
-              answer:
-                'Jsem sportovní nadšenec a dám si do těla minimálně 5x týdně.',
-            },
-          ],
-        },
-        {
-          question: 'Jaké je Vaše zaměstnání, popřípadě náplň dne?',
-          choices: [
-            {
-              option: 'A',
-              answer: 'Nejsem aktivní. Denně ujdu méně než 8 K kroků.',
-            },
-            {
-              option: 'B',
-              answer: 'Jsem středně aktivní, nachodím kolem 8 K kroků.',
-            },
-            {
-              option: 'C',
-              answer:
-                'Jsem aktivní a většinu dne na nohou. Nachodím nad 10 K kroků',
-            },
-          ],
-        },
-      ],
-    },
   });
   const nextStep = () => setStep((prev) => prev + 1);
   const prevStep = () => setStep((prev) => prev - 1);
@@ -82,6 +40,7 @@ export const FormParent = () => {
       return (
         <FormActivity
           formData={formData}
+          //questions={questions}
           setFormData={setFormData}
           nextStep={nextStep}
           prevStep={prevStep}
