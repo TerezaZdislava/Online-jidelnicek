f34d6cf86d1d05588c8c28feb477977c290ccf59
Daniela Kůsová <danielka.kusova@gmail.com>
2020-12-05 13:36:21 +0100

    footer final

--- src/Footer.js
+++ src/Footer.js
@@ -2,15 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter = styled.div`
-  .footer {
     display: flex;
-    height: auto;
+    height: 60px;
     justify-content: space-around;
     width: 100%;
     box-shadow: 0px 5px 4px 0px rgba(50, 50, 50, 0.3);
-  }
-  .logo {
-    justify-content: wrap;
   }
   .logo2 {
     color: rgb(255, 0, 55);
@@ -27,17 +23,15 @@ const Footer = () => {
   return (
     <StyledFooter>
       <div className="footer">
-        <span className="copyright">Copyright©2O2O</span>
-        <div className="logo">
-          <span className="logo1">ONLINE</span>
-          <span className="logo2">JÍDELNÍČEK</span>
-        </div>
-        <div className="e-mail">
-          V případě dotazů či spolupráce nás kontaktuj:{' '}
-          <a href={`mailto:${`info@online-jidelnicek.cz`}`}>
-            info@online-jidelnicek.cz
-          </a>
-        </div>
+        <span className="copyright">Copyright©2O2O, </span>
+        <span className="logo1">ONLINE</span>
+        <span className="logo2">JÍDELNÍČEK</span>
+      </div>
+      <div className="e-mail">
+        V případě dotazů či spolupráce nás kontaktuj:{' '}
+        <a href={`mailto:${`info@online-jidelnicek.cz`}`}>
+          info@online-jidelnicek.cz
+        </a>
       </div>
     </StyledFooter>
   );
