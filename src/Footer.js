b748dddfa5c1121656ddcc34a3132b372ef82818
Daniela Kůsová <danielka.kusova@gmail.com>
2020-12-05 11:22:48 +0100

    Footer better

--- src/Footer.js
+++ src/Footer.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledFooter = styled.footer`
-  display: block;
-  margin-left: 25%;
-  width: 100%;
-  box-shadow: 0px 5px 4px 0px rgba(50, 50, 50, 0.3);
-  height: 60px;
-  h5 {
-    margin: 0;
-    height: 60px;
+const StyledFooter = styled.div`
+  .footer {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    padding: 1 rem;
     box-shadow: 0px 5px 4px 0px rgba(50, 50, 50, 0.3);
+    height: auto;
+  }
+  .logo {
+    justify-content: wrap;
+  }
+  .logo2 {
+    color: rgb(255, 0, 55);
+    margin: 0;
+    font-weight: 700;
+  }
+  .logo1 {
+    color: black;
+    margin: 0;
   }
 `;
 
 const Footer = () => {
   return (
     <StyledFooter>
-      <h5>
-        Copyright©2O2O, Online-jidelnicek.cz V případě dotazů nás kontaktujte:{' '}
-        <e-mail>info@online-jidelnicek.cz</e-mail>
-      </h5>
+      <div className="footer">
+        <span className="copyright">Copyright©2O2O,</span>
+        <div className="logo">
+          <span className="logo1">ONLINE</span>
+          <span className="logo2">JÍDELNÍČEK</span>
+        </div>
+        <span className="e-mail">
+          V případě dotazů nás kontaktujte:{' '}
+          <e-mail>info@online-jidelnicek.cz</e-mail>
+        </span>
+      </div>
     </StyledFooter>
   );
 };
