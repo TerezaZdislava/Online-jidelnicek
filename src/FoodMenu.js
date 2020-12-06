13c90ed47c20d5cbb2dfbec9a984320a66f246f8
Daniela Kůsová <danielka.kusova@gmail.com>
2020-12-06 11:41:33 +0100

    Button FoodMenu pořešené

--- src/FoodMenu.js
+++ src/FoodMenu.js
@@ -86,24 +86,29 @@ const StyledSection = styled.section`
   }
 `;
 const StyledDiv = styled.div`
+  background-image: url(${(props) => props.img});
+  background-size: cover;
   padding: 1rem;
-  height: auto;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 1px 1px 16px 0px rgba(50, 50, 50, 0.75);
-  background-color: white;
-  h1 {
-    color: black;
-    font-size: 2rem;
-  }
 
+  .whyMenu {
+    padding: 1rem;
+    height: auto;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 1px 1px 16px 0px rgba(50, 50, 50, 0.75);
+  }
+  h1 {
+    color: white;
+    font-size: 1rem;
+  }
+.buttonArea {
+  display:flex;
+  justify-content: center;
+  alig-items: center;
+  padding:1rem;
+}
   button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
     background-color: rgb(255, 0, 55);
-    margin-top: 10 rem;
-    margin-left: 3rem;
     width: 170px;
     height: 40px;
     border-radius: 5px;
@@ -114,23 +119,23 @@ const StyledDiv = styled.div`
   }
 
   @media (min-width: 550px) {
-    margin-left: 15%;
-    margin-right: 15%;
+    padding-left: 15%;
+    padding-right: 15%;
   }
   @media (min-width: 750px) {
     button {
       cursor: pointer;
     }
-    margin-left: 25%;
-    margin-right: 25%;
+    padding-left: 25%;
+    padding-right: 25%;
     font-size: 1.3rem;
-    h5 {
+    h1 {
       font-size: 1.5rem;
     }
   }
   @media (min-width: 1200px) {
-    margin-left: 30%;
-    margin-right: 30%;
+    padding-left: 30%;
+    padding-right: 30%;
   }
 `;
 
@@ -140,28 +145,32 @@ const FoodMenu = () => {
 
   if (jsonForm == null) {
     return (
-      <StyledDiv>
+      <StyledDiv img={`./assets/img/foodimage-form.jpg`}>
         <h1>Online generátor jídelníčku</h1>
-        <p>Vyzkoušej to!</p>
-        <p>Za 2 minuty můžeš mít svůj jídelníček:</p>
-        <p>
-          👉 Jídelníček na základě dat o tobě vypočítá hodnotu kalorií pro
-          správné fungování tvého bazálního metabolismu.
-        </p>
-        <p>
-          👉 Jídelníček bere v úvahu i to, jak jsi přes den aktivní a jestli
-          sportuješ. To nám pomůže zjistit ideální denní příjem kalorií.
-        </p>
-        <p>
-          👉 Generátor je výjimečný v tom, že si můžeš z jídelníčku odstranit
-          potraviny, které nejíš. Také si stanovíš, kolik jídel za den chceš.
-        </p>
-        <p>
-          Aplikace Ti navrhne na míru chutný jídelníček zatím na jeden den. Ale
-          plánujeme brzy rozšíření na celý měsíc! Využij tedy možnost si
-          postavit jídelníček zatím zdarma.
-        </p>
-        <button onClick={() => history.push(`/form`)}>Chci jídelníček</button>
+        <div className="whyMenu">
+          <p>Vyzkoušej to!</p>
+          <p>Za 2 minuty můžeš mít svůj jídelníček:</p>
+          <p>
+            👉 Jídelníček na základě dat o tobě vypočítá hodnotu kalorií pro
+            správné fungování tvého bazálního metabolismu.
+          </p>
+          <p>
+            👉 Jídelníček bere v úvahu i to, jak jsi přes den aktivní a jestli
+            sportuješ. To nám pomůže zjistit ideální denní příjem kalorií.
+          </p>
+          <p>
+            👉 Generátor je výjimečný v tom, že si můžeš z jídelníčku odstranit
+            potraviny, které nejíš. Také si stanovíš, kolik jídel za den chceš.
+          </p>
+          <p>
+            Aplikace Ti navrhne na míru chutný jídelníček zatím na jeden den.
+            Ale plánujeme brzy rozšíření na celý měsíc! Využij tedy možnost si
+            postavit jídelníček zatím zdarma.
+          </p>
+        </div>
+        <div className="buttonArea">
+          <button onClick={() => history.push(`/form`)}>Chci jídelníček</button>
+        </div>
       </StyledDiv>
     );
   }
